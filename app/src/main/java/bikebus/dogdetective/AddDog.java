c38542daf87bc3d46ecff40b870429239f98deb5
Tal Usvyatsky <usvyatsky.t@husky.neu.edu>
2018-02-10 17:38:38 -0500

    Trying to make add dog buttons look more button-like when pressed

--- app/src/main/java/bikebus/dogdetective/AddDog.java
+++ app/src/main/java/bikebus/dogdetective/AddDog.java
@@ -1,13 +1,68 @@
 package bikebus.dogdetective;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class AddDog extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_dog);
+
+
+
+        ImageButton dog1 = (ImageButton) findViewById(R.id.WhiteDogLightBlueBack);
+        ImageButton dog2 = (ImageButton) findViewById(R.id.TanDogGreenBack);
+        ImageButton dog3 = (ImageButton) findViewById(R.id.BrownDogYellowBack);
+        ImageButton dog4 = (ImageButton) findViewById(R.id.GrayDogDarkBlueBack);
+        ImageButton dog5 = (ImageButton) findViewById(R.id.BrownDogPurpleBack);
+        ImageButton dog6 = (ImageButton) findViewById(R.id.GrayDogOrangeBack);
+
+        dog1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddDog.this, AddTags.class));
+            }
+        });
+        dog2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddDog.this, AddTags.class));
+            }
+        });
+        dog3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddDog.this, AddTags.class));
+            }
+        });
+        dog4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddDog.this, AddTags.class));
+            }
+        });
+        dog5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddDog.this, AddTags.class));
+            }
+        });
+        dog6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddDog.this, AddTags.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/bikebus/dogdetective/AddTags.java
@@ -0,0 +1,13 @@
+package bikebus.dogdetective;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTags extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tags);
+    }
+}
