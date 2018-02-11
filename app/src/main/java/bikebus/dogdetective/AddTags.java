9942564d98ef843ed47d99b34aa4c226c9539a48
Tal Usvyatsky <usvyatsky.t@husky.neu.edu>
2018-02-10 20:48:49 -0500

    added tag screen

--- app/src/main/java/bikebus/dogdetective/AddTags.java
+++ app/src/main/java/bikebus/dogdetective/AddTags.java
@@ -1,13 +1,44 @@
 package bikebus.dogdetective;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.google.android.flexbox.FlexboxLayout;
 
 public class AddTags extends AppCompatActivity {
+
+    private String[] dogWords = {"floof", "pupperino", "doggo", "12/10", "smol", "medium", "big",
+            "moon moon", "bork", "11/10", "good", "great", "wow", "heckin' amaze", "13/10", "mlem",
+    "blep", "corgo", "puggo", "good boye", "good gurl"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_tags);
+
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        FlexboxLayout flexboxLayout = (FlexboxLayout) findViewById(R.id.flexbox);
+      //  FlexboxLayout.LayoutParams layoutParams = (FlexboxLayout.LayoutParams) flexboxLayout.getLayoutParams();
+      //  layoutParams.setMargins(20, 20, 20, 20);
+
+        for(String s: dogWords) {
+            FrameLayout view = (FrameLayout) inflater.inflate(R.layout.button_tag, null);
+
+            ((Button)view.findViewById(R.id.innerButton)).setText(s);
+            flexboxLayout.addView(view);
+            view.invalidate();
+
+
+        }
+        flexboxLayout.invalidate();
+
     }
+
 }
